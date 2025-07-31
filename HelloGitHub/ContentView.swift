80babe794c6f0557d229b5d88d0c4aa2b632d740
sgmorita <shin.gaia.morita@gmail.com>
2025-07-31 15:44:38 +0900

    Initial Commit

---
+++ HelloGitHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloGitHub
+//
+//  Created by 慎 on 2025/07/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloGitHub/HelloGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloGitHubApp.swift
+//  HelloGitHub
+//
+//  Created by 慎 on 2025/07/31.
+//
+
+import SwiftUI
+
+@main
+struct HelloGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
