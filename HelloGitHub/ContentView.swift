ba28a1fc510db84e1cad7a730ae0749204f1fd37
Shin Morita <shin.gaia.morita@gmail.com>
2025-07-31 17:05:57 +0900

    ABC

--- HelloGitHub/ContentView.swift
+++ HelloGitHub/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, GitHuuuuuub!!!")
+            Text("Hello, GH!")
         }
         .padding()
     }
